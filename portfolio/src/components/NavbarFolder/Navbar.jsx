64bb0c1838e0d35949994450af29b352c3bc7d1c
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2023-01-27 23:40:12 -0500

    Update Navbar.jsx

--- portfolio/src/components/NavbarFolder/Navbar.jsx
+++ portfolio/src/components/NavbarFolder/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import './Navbar.css'
+import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
+import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
 
 const Navbar = () => {
   return (
-    <Navbar>
-      
-    </Navbar>
+   <div>
+    <a href="#home"><AiOutlineHome/></a>
+    <a href="#about"><AiOutlineUser/></a>
+    <a href="#experience"><BiBook/></a>
+    <a href="#services"><RiServiceLine/></a>
+    <a href="#contact"><BiMessageSquareDetail/></a>
+   </div>
   )
 }
 
