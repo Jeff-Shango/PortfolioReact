0e84d8d6a6726fba40b2c985c31cc07696bc4f0e
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2023-01-27 19:54:49 -0500

    updating

--- portfolio/src/components/HeaderFolder/Header.jsx
+++ portfolio/src/components/HeaderFolder/Header.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import './Header.css'
+import Resume from './CTA'
+import profilePic from '../../assets/profilepic.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <Header>
-      <div className="container header_container">
+    <header>
+      <div className='container header_container'>
         <h5>Hello I'm</h5>
         <h1>Jeff Bozier</h1>
+        <h5 className="text-light">Software Engineer</h5>
+        <Resume /> 
+        <HeaderSocials />
+
+        <div className="me">
+            <img src={profilePic} alt="Profile Pic" />
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
       </div>
-    </Header>
+    </header>
   )
 }
 
